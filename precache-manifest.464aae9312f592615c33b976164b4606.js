7fb406a7013a31c5ee98762d174fd016208591d3
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-10-01 18:34:30 -0400

    Updates

---
+++ precache-manifest.464aae9312f592615c33b976164b4606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30f73592e4bbeb939db3ddce90ea231",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "b6a5fd65650d189e0ed1",
+    "url": "/react-clicky-game/static/css/main.63d51184.chunk.css"
+  },
+  {
+    "revision": "5aa4f2a5f3a6086c9ccd",
+    "url": "/react-clicky-game/static/js/2.4b3c70c9.chunk.js"
+  },
+  {
+    "revision": "b6a5fd65650d189e0ed1",
+    "url": "/react-clicky-game/static/js/main.f3769866.chunk.js"
+  },
+  {
+    "revision": "c44a2b1e3312ad9f1be0",
+    "url": "/react-clicky-game/static/js/runtime-main.a31f8eb9.js"
+  }
+]);
